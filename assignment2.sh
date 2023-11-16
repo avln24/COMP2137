c5d9e8fbe664950723422984e19c49260e43e796
Alaine Naidas <avnaidas@gmail.com>
2023-11-16 18:52:24 -0500

    Updated assignment2.sh with formatting changes

--- assignment2.sh
+++ assignment2.sh
@@ -9,6 +9,8 @@
 echo "Checking for sudo"
 if [ "$(id -u)" -eq 0 ]; then
     echo "Running system modification script."
+    echo "------------------------------------"
+    echo ""
 else
     echo "User does not have sudo permissions. Cannot run script."
     exit 1
@@ -20,6 +22,9 @@ static_ip="192.168.16.21/24"
 gateway_ip="192.168.16.1"
 dnsserver_ip="192.168.16.1"
 dns_searchdomain="home.arpa localdomain"
+
+echo "Configuring network interfaces"
+echo "------------------------------------"
 
 # Get the name of the second interface (host-only) and make sure it exists
 hostonly_interface=$(ip a | grep -E "ens|eth" | grep "inet" | awk 'FNR==2 {print $7}')
@@ -103,8 +108,14 @@ fi
 
 #Check if openssh, apache2, and squid web proxy are already installed
 #If package is not installed, install it
+echo ""
+echo "Installing required software"
+echo "------------------------------------"
 
 packages="openssh-server apache2 squid"
+
+echo "Running apt update. Please wait..."
+apt update > /dev/null 2>&1
 
 for package in $packages; do
     dpkg-query -s $package 2> /dev/null | grep "installed" > /dev/null 2>&1  
@@ -124,7 +135,9 @@ done
 
 #CONFIGURE OPENSSH-SERVER
 #Openssh-server allowing ssh key authentication and not allowing password authentication
-
+echo ""
+echo "Configuring OpenSSH server"
+echo "------------------------------------"
 ssh_config="/etc/ssh/sshd_config"
 echo "Checking if OpenSSH server public key authentication is enabled"
 if [ $(grep -i "PubkeyAuthentication" $ssh_config | awk '{print $2}') = "yes" ]; then
@@ -173,6 +186,10 @@ sudo systemctl restart ssh && echo "Restarted SSH service."
 #CONFIGURE APACHE2
 #Apache2 web server listening for http on port 80 and https on port 443
 #Check /etc/apache2/ports.conf if Apache2 web server is listening to port 80
+echo ""
+echo "Configuring Apache2 web server"
+echo "------------------------------------"
+
 grep -iw 'Listen 80' /etc/apache2/ports.conf > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Apache2 web server is already listening to port 80"
@@ -215,7 +232,12 @@ echo "Reloading Apache2 web service"
 systemctl reload apache2
 
 #CONFIGURE SQUID WEB PROXY
+echo ""
+echo "Configuring Squid web proxy"
+echo "------------------------------------"
+
 #Squid web proxy listening on port 3128
+echo "Checking what port squid web proxy is listening to..."
 grep -iw 'http_port 3128' /etc/squid/squid.conf > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Squid web proxy is already listening to port 3128"
@@ -225,7 +247,12 @@ else
 fi
 
 ##### ENABLE FIREWALL AND ADD UFW RULES #####
+echo ""
+echo "Configuring firewall"
+echo "------------------------------------"
+
 #Check if firewall is enabled:
+echo "Checking firewall status..."
 ufw status | grep "Status: active"
 if [ $? -eq 0 ]; then
     echo "Firewall is already active. Skipping to next step."
@@ -237,6 +264,7 @@ fi
 #Check if ufw rule already exists, if it does not exist then add it.
 ports="22 80 443 3128"
 
+echo "Adding UFW rules..."
 for port in $ports; do
     ufw status | grep "$port" | grep "ALLOW" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
@@ -246,7 +274,175 @@ for port in $ports; do
         ufw allow $port/tcp
     fi
 done
-    
+
+##### CREATE USER ACCOUNTS #####
+echo ""
+echo "Creating and configuring user accounts"
+echo "------------------------------------"
+#Check if user "dennis" already exists
+echo ""
+echo "Checking if user: dennis already exists..."
+grep -w "dennis" /etc/passwd > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "User: dennis does not exist. Creating user: dennis (sudo access)"
+    useradd -s /bin/bash -m -G sudo dennis 
+    grep -w "dennis" /etc/passwd > /dev/null 2>&1 && echo "User: dennis was successfully created."
+else
+    echo "User: dennis already exists! Skipping user creation."
+fi
+
+#Check if ~/.ssh exists in /home/dennis/, if it does not exist then create it
+echo ""
+echo "Configuring SSH for dennis"
+find /home/dennis/.ssh > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "/home/dennis/.ssh exists!"
+else
+    echo "Could not find /home/dennis/.ssh. Creating ~/.ssh directory."
+    mkdir /home/dennis/.ssh
+    chmod 700 /home/dennis/.ssh
+fi
+
+#Check if authorized_keys file exists. Create it if it does not.
+find /home/dennis/.ssh/authorized_keys > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Authorized_keys file already exists inside /home/dennis/.ssh/."
+else
+    echo "Authorized_keys file not found. Creating ~/.ssh/authorized_keys file..."
+    touch /home/dennis/.ssh/authorized_keys
+    chmod 600 /home/dennis/.ssh/authorized_keys
+    find /home/dennis/.ssh/authorized_keys > /dev/null 2>&1 && echo "Authorized_keys file created in /home/dennis/.ssh/."
+fi
+
+
+#Add SSH-ED25519 public key for student@generic-vm to ~/.ssh directory for user dennis
+touch /home/dennis/.ssh/id_ed25519_genericvm.pub
+chmod 644 /home/dennis/.ssh/id_ed25519_genericvm.pub
+cat > /home/dennis/.ssh/id_ed25519_genericvm.pub <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4rT3vTt99Ox5kndS4HmgTrKBT8SKzhK4rhGkEVGlCI student@generic-vm
+EOF
+if [ $? -eq 0 ]; then
+    echo "Added SSH-ED25519 public key for student@generic-vm to /home/dennis/.ssh/"
+else
+    echo "Could not add SSH-ED25519 public key for student@generic-vm to /home/dennis/.ssh/. Exiting script."
+    exit 1
+fi
+
+#Add SSH-ED25519 public key for student@generic-vm to authorized keys file
+cat /home/dennis/.ssh/id_ed25519_genericvm.pub >> /home/dennis/.ssh/authorized_keys
+if [ $? -eq 0 ]; then
+    echo "Added SSH-ED25519 public key for student@generic-vm to /home/dennis/.ssh/authorized_keys"
+else
+    echo "Could not add SSH-ED25519 public key for student@generic-vm to /home/dennis/.ssh/authorized_keys. Exiting script."
+    exit 1
+fi
+
+### CONFIGURE ALL USER ACCOUNTS ### 
+regusers="aubrey captain snibbles brownie scooter sandy perrier cindy tiger yoda"
+allusers="aubrey captain snibbles brownie scooter sandy perrier cindy tiger yoda dennis"
+
+echo ""
+for username in $regusers; do
+    #Check if user already exists and add them if they do not yet exist
+    echo "Checking if user: $username already exists..."
+    grep -w $username /etc/passwd > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "User: $username does not exist. Creating user!"
+        useradd -s /bin/bash -m $username 
+        grep -w $username /etc/passwd > /dev/null 2>&1 && echo "User: $username was successfully created."
+    else
+        echo "User: $username already exists! Skipping user creation."
+    fi
+done
+
+for username in $allusers; do
+    echo ""
+    echo "Configuring SSH for $username"
+    #Check if ~/.ssh exists in /home/$username/, if it does not exist then create it
+    echo "Checking if ~/.ssh directory exists for $username..."
+    find /home/$username/.ssh > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "/home/$username/.ssh exists!\n"
+    else
+        echo "Could not find /home/$username/.ssh. Creating ~/.ssh directory."
+        mkdir /home/$username/.ssh
+        chmod 700 /home/$username/.ssh
+    fi
+
+    #Check if user already has SSH keys for RSA.
+    echo ""
+    echo "Checking if SSH RSA key pair already exists..."
+    find /home/$username/.ssh/id_rsa.pub > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "SSH RSA Public Key already exists for $username. Skipping generation of SSH RSA keys."
+    else
+        echo "SSH RSA Public key does not exist. Generating SSH RSA keys for $username..."
+        ssh-keygen -t rsa -q -N '' -f /home/$username/.ssh/id_rsa <<<y
+        if [ $? -eq 0 ]; then
+            find /home/$username/.ssh/id_rsa.pub > /dev/null 2>&1 && echo "SSH RSA key pair generated successfully for $username."
+        else
+            echo "Failed to generate SSH-RSA key pair for $username. Exiting script."
+            exit 1
+        fi
+    fi
+
+    #Check if user already has SSH keys for ED25519 algorithm.
+    echo ""
+    echo "Checking if SSH-ED25519 key pair already exists..."
+    find /home/$username/.ssh/id_ed25519.pub > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "SSH ED25519 Public Key already exists for $username. Skipping generation of SSH ED25519 keys."
+    else
+        echo "SSH ED25519 Public key does not exist. Generating SSH ED25519 keys for $username..."
+        ssh-keygen -t ed25519 -q -N '' -f /home/$username/.ssh/id_ed25519 <<<y
+        if [ $? -eq 0 ]; then
+            find /home/$username/.ssh/id_ed25519.pub > /dev/null 2>&1 && echo "SSH ED25519 key pair generated successfully for $username."
+        else
+            echo "Failed to generate SSH-ED25519 key pair for $username. Exiting script."
+            exit 1
+        fi
+    fi
+
+    #Check if user has authorized keys file. Create it, if it does not exist.
+    echo ""
+    echo "Checking if authorized_keys file already exists..."
+    find /home/$username/.ssh/authorized_keys > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Authorized_keys file already exists."
+    else
+        echo "Authorized_keys file not found. Creating ~/.ssh/authorized_keys file..."
+        touch /home/$username/.ssh/authorized_keys
+        chmod 600 /home/$username/.ssh/authorized_keys
+        find /home/$username/.ssh/authorized_keys > /dev/null 2>&1 && echo "Authorized_keys file created."
+    fi
+
+    #Check if authorized_keys file contains SSH-RSA public key.
+    echo ""
+    echo "Checking if authorized_keys file contains SSH RSA public key..."
+    grep -i "ssh-rsa" /home/$username/.ssh/authorized_keys > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "SSH-RSA public key already exists within authorized_keys file. Skipping this step."
+    else
+        echo "SSH-RSA public key not found within authorized_keys file. Adding public key to file..."
+        cat /home/$username/.ssh/id_rsa.pub >> /home/$username/.ssh/authorized_keys
+        grep -i "ssh-rsa" /home/$username/.ssh/authorized_keys > /dev/null 2>&1 && echo "SSH-RSA public key added to authorized_keys file."
+    fi
+
+    #Check if authorized_keys file contains SSH-ED25519 public key.
+    echo ""
+    echo "Checking if authorized_keys file contains SSH ED25519 public key..."
+    grep -i "ssh-ed25519" /home/$username/.ssh/authorized_keys > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "SSH-ED25519 public key already exists within authorized_keys file. Skipping this step."
+    else
+        echo "SSH-ED25519 public key not found within authorized_keys file. Adding public key to file..."
+        cat /home/$username/.ssh/id_ed25519.pub >> /home/$username/.ssh/authorized_keys
+        grep -i "ssh-ed25519" /home/$username/.ssh/authorized_keys > /dev/null 2>&1 && echo "SSH-ED25519 public key added to authorized_keys file."
+    fi
+done
+
+echo "Finished making changes to system configurations!"
+
 
 
 
