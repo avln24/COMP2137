9f01cff292b10691249d4190cd932a4117c08462
Alaine Naidas <avnaidas@gmail.com>
2023-11-27 23:26:05 -0500

    Updated assignment3.sh

--- assignment3.sh
+++ assignment3.sh
@@ -8,36 +8,66 @@
  
 #Change system name from target1 to loghost (both hostname and /etc/hosts file)
 #Check if hostname is already set to loghost, and change it if is not
-ssh remoteadmin@target1-mgmt << EOF
+ssh remoteadmin@server1-mgmt << EOF
 
 if [ "$(hostname)" = "loghost" ]; then
     echo "System name is already configured as "loghost", skipping step"
 else
     echo "Changing system name to "loghost"..."
-    hostnamectl set-hostname loghost
-    grep "loghost" /etc/hostname
-    if [ $? -eq 0 ]; then
-        echo "System name was successfully changed to loghost."
-    else
-        echo "Failed to change system name to "loghost"."
+    hostnamectl set-hostname loghost 
+    grep "loghost" /etc/hostname && echo "System name was successfully changed to loghost." || echo "Failed to change system name to loghost."
 fi
+EOF
 
 #Check if "loghost" exists inside /etc/hosts file and add it if it does not
-
-grep -w "loghost" /etc/hosts
+ssh remoteadmin@server1-mgmt << EOF
+grep -w "loghost" /etc/hosts > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "/etc/hosts already contains loghost"
+    echo "hostname already configured to loghost within /etc/hosts file"
 else
     echo "Changing hostname to loghost within /etc/hosts file..."
     grep "server1" /etc/hosts | sed -i 's/server1/loghost/g' /etc/hosts 
-    grep -E "loghost|loghost-mgmt" && echo "Hostname was changed to loghost within /etc/hosts" || echo "Failed to change hostname to loghost in /etc/hosts"
-fi    
-
+    grep -E "loghost" /etc/hosts && echo "Hostname was changed to loghost within /etc/hosts" || echo "Failed to change hostname to loghost in /etc/hosts"
+fi   
 EOF
 
 #Change IP Address from host 10 to host 3 on the LAN
+ssh remoteadmin@server1-mgmt << EOF
+
+yaml_file=$(find /etc/netplan -type f -name '*.yaml')
+
+grep "192.168.16.10/24" $yaml_file | sed -i 's/192.168.16.10/192.168.16.3/' $yaml_file
+netplan apply > /dev/null 2>&1
+new_ip=$(hostname -I | awk '{print $1}')
+if [ "$new_ip" = "192.168.16.3" ]; then
+    echo "IP address was successfully configured to: 192.168.16.3"
+else
+    echo "Failed to change IP address to host 3 on LAN"
+fi
+
+EOF
 
 #Add a machine named webhost to the /etc/hosts file as host 4 on the LAN
+ssh remoteadmin@server1-mgmt << EOF
+
+grep "webhost" /etc/hosts
+if [ $? -eq 0 ]; then
+    echo "webhost already exists within /etc/hosts file. Checking if IP address is configured correctly..."
+    
+    grep "192.168.16.4" /etc/hosts > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "IP address for webhost is already configured correctly."
+    else
+        grep "webhost" /etc/hosts | sed -i 's/.*webhost/192.168.16.4 webhost/' /etc/hosts 
+        grep "192.168.16.4 webhost" /etc/hosts > /dev/null 2>&1 && echo "webhost exists in /etc/hosts." || echo "Failed to add correct webhost IP address to /etc/hosts file"
+    fi
+
+else
+    echo "192.168.16.4 webhost" >> /etc/hosts
+    grep "192.168.16.4 webhost" /etc/hosts > /dev/null 2>&1 && echo "webhost added to /etc/hosts." || echo "Failed to add webhost to /etc/hosts file"
+fi
+
+EOF
 
 # Install UFW (if necessary) and allow connections to port 514/udp from the MGMT network
 
