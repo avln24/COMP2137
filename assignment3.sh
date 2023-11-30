604d8ef262d2f38aba12c0950a7621219f17871d
Alaine Naidas <avnaidas@gmail.com>
2023-11-29 21:24:16 -0500

    Updated assignment3.sh

--- assignment3.sh
+++ assignment3.sh
@@ -5,7 +5,7 @@
 #NOTE: your script must only use ssh to access the target1 and target2 machines
 
 ##### Configuring Target1-mgmt (172.16.1.10) #####
-ssh_server1mgmt=$(ssh -o StrictHostKeyChecking=no remoteadmin@server1-mgmt)
+ssh remoteadmin@server1-mgmt "echo 'Configuring Target1'"
 
 #Change system name from target1 to loghost (both hostname and /etc/hosts file)
 #Check if hostname is already set to loghost, and change it if is not
@@ -14,9 +14,15 @@ ssh remoteadmin@server1-mgmt << EOF
 if [ "$(hostname)" = "loghost" ]; then
     echo "System name is already configured as "loghost", skipping step"
 else
-    echo "Changing system name to "loghost"..."
+    echo "Changing system name to 'loghost'..."
     hostnamectl set-hostname loghost 
-    grep "loghost" /etc/hostname && echo "System name was successfully changed to loghost." || echo "Failed to change system name to loghost."
+    grep "loghost" /etc/hostname
+    if [ $? -eq 0 ]; then
+        echo "System name was successfully changed to loghost."
+    else
+        echo "Failed to change system name to loghost."
+        exit 1
+    fi
 fi
 EOF
 
@@ -28,44 +34,63 @@ if [ $? -eq 0 ]; then
 else
     echo "Changing hostname to loghost within /etc/hosts file..."
     grep "server1" /etc/hosts | sed -i 's/server1/loghost/g' /etc/hosts 
-    grep -E "loghost" /etc/hosts && echo "Hostname was changed to loghost within /etc/hosts" || echo "Failed to change hostname to loghost in /etc/hosts"
+    grep "loghost" /etc/hosts
+    if [ $? -eq 0 ]; then 
+        echo "Hostname was changed to loghost within /etc/hosts!"
+    else
+        echo "Failed to change hostname to loghost in /etc/hosts."
+        exit 1
+    fi
 fi   
 EOF
 
 #Change IP Address from host 10 to host 3 on the LAN
 ssh remoteadmin@server1-mgmt << EOF
 
+lan_netip=$(hostname -I | awk '{ print $1 }' | sed 's/.10$//') 
 yaml_file=$(find /etc/netplan -type f -name '*.yaml')
 
-grep "192.168.16.10/24" $yaml_file | sed -i 's/192.168.16.10/192.168.16.3/' $yaml_file
+grep "$lan_netip.10/24" $yaml_file | sed -i "s/$lan_netip.10/$lan_netip.3/" $yaml_file
 netplan apply > /dev/null 2>&1
-new_ip=$(hostname -I | awk '{print $1}')
-if [ "$new_ip" = "192.168.16.3" ]; then
-    echo "IP address was successfully configured to: 192.168.16.3"
+new_ip=$(hostname -I | awk '{ print $1 }')
+if [ "$new_ip" = "$lan_netip.3" ]; then
+    echo "IP address was successfully configured to host 3 on LAN"
 else
     echo "Failed to change IP address to host 3 on LAN"
+    exit 1
 fi
 
 EOF
 
 #Add a machine named webhost to the /etc/hosts file as host 4 on the LAN
 ssh remoteadmin@server1-mgmt << EOF
+lan_netip=$(hostname -I | awk '{ print $1 }' | sed 's/.3$//')
 
 grep "webhost" /etc/hosts
 if [ $? -eq 0 ]; then
     echo "webhost already exists within /etc/hosts file. Checking if IP address is configured correctly..."
-    
-    grep "192.168.16.4" /etc/hosts > /dev/null 2>&1
+    grep "$lan_netip.4" /etc/hosts > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "IP address for webhost is already configured correctly."
     else
-        grep "webhost" /etc/hosts | sed -i 's/.*webhost/192.168.16.4 webhost/' /etc/hosts 
-        grep "192.168.16.4 webhost" /etc/hosts > /dev/null 2>&1 && echo "webhost exists in /etc/hosts." || echo "Failed to add correct webhost IP address to /etc/hosts file"
+        grep "webhost" /etc/hosts | sed -i "s/.*webhost/$lan_netip.4 webhost/" /etc/hosts 
+        grep "$lan_netip.4 webhost" /etc/hosts > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "webhost exists in /etc/hosts."
+        else
+            echo "Failed to add correct webhost IP address to /etc/hosts file"
+            exit 1
+        fi
     fi
-
 else
-    echo "192.168.16.4 webhost" >> /etc/hosts
-    grep "192.168.16.4 webhost" /etc/hosts > /dev/null 2>&1 && echo "webhost added to /etc/hosts." || echo "Failed to add webhost to /etc/hosts file"
+    echo "$lan_netip.4 webhost" >> /etc/hosts
+    grep "$lan_netip.4 webhost" /etc/hosts > /dev/null 2>&1 
+    if [ $? -eq 0 ]; then
+        echo "webhost added to /etc/hosts."
+    else
+        echo "Failed to add webhost to /etc/hosts file"
+        exit 1
+    fi
 fi
 
 EOF
@@ -83,7 +108,7 @@ else
 fi
 
 ufw_status=$(ufw status | grep "Status:" | awk '{ print $2 }')
-if [ "$ufw_status" = "active"]; then
+if [ "$ufw_status" = "active" ]; then
     echo "UFW already enabled!"
 else
     ufw --force enable
@@ -91,10 +116,10 @@ fi
 
 ufw status | grep "514/udp" | grep "ALLOW" | grep "172.16.1.0"
 if [ $? -eq 0 ]; then
-    echo "UFW Rule already exists: Allow connections to 514/udp from MGMT network"
+    echo "UFW Rule already exists: allow connections to 514/udp from MGMT network"
 else
     echo "Adding UFW Rule: allow connections to port 514/UDP from MGMT network!"
-    ufw allow proto udp from 172.16.1.0/24 to any port 514
+    ufw allow from 172.16.1.0/24 to any port 514 proto udp
 fi
 
 EOF
@@ -105,44 +130,196 @@ EOF
 #Restart the rsyslog service using systemctl restart rsyslog
 ssh remoteadmin@server1-mgmt << EOF
 
-grep "imudp" /etc/rsyslog.conf | grep "#" > /dev/null
-if [ $? -eq 0 ]; then
+grep "imudp" /etc/rsyslog.conf | grep "#" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
     echo "rsyslog is already listening for UDP connections"
 else
     echo "Configuring rsyslog to listen for UDP connections..."
-    grep "imudp" /etc/rsyslog.conf | sed -i 's/#//g' /etc/rsyslog.conf
+    grep "imudp" /etc/rsyslog.conf | sed -i 's/#//' /etc/rsyslog.conf
+    systemctl restart rsyslog
 fi
-
-systemctl restart rsyslog
-
 EOF
 
 ##### Configuring Target2-mgmt (172.16.1.11) #####
 
+ssh remoteadmin@server1-mgmt "echo 'Configuring Target2'"
+
 #Change system name from target 2 to webhost (both hostname and /etc/hosts file)
+ssh remoteadmin@server2-mgmt << EOF
+
+if [ "$(hostname)" = "webhost" ]; then
+    echo "System name is already configured as "webhost", skipping step"
+else
+    echo "Changing system name to 'webhost'..."
+    hostnamectl set-hostname webhost 
+    grep "webhost" /etc/hostname
+    if [ $? -eq 0 ]; then
+        echo "System name was successfully changed to webhost."
+    else
+        echo "Failed to change system name to loghost."
+        exit 1
+    fi
+fi
+
+EOF
 
 #Change IP Address from host 11 to host 4 on the LAN
+ssh remoteadmin@server2-mgmt << EOF
+
+lan_netip=$(hostname -I | awk '{ print $1 }' | sed 's/.11$//')
+yaml_file=$(find /etc/netplan -type f -name '*.yaml')
+
+grep "$lan_netip.11/24" $yaml_file | sed -i "s/$lan_netip.11/$lan_netip.4/" $yaml_file
+netplan apply > /dev/null 2>&1
+new_ip=$(hostname -I | awk '{ print $1 }')
+if [ "$new_ip" = "$lan_netip.4" ]; then
+    echo "IP address was successfully configured to host 4 on LAN"
+else
+    echo "Failed to change IP address to host 4 on LAN"
+    exit 1
+fi
+
+EOF
 
 #Add a machine named loghost to the /etc/hosts file as host 3 on the LAN
 
+ssh remoteadmin@server2-mgmt << EOF
+
+grep "loghost" /etc/hosts > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "loghost already exists within /etc/hosts file. Checking if IP address is configured correctly..."
+    grep "$lan_netip.3" /etc/hosts > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "IP address for loghost is already configured correctly."
+    else
+        grep "loghost" /etc/hosts | sed -i "s/.*loghost/$lan_netip.3 loghost/" /etc/hosts 
+        grep "$lan_netip.3 loghost" /etc/hosts > /dev/null 2>&1 
+        if [ $? -eq 0 ]; then
+            echo "loghost has the correct IP address in /etc/hosts."
+        else
+            echo "Failed to add correct loghost IP address to /etc/hosts file"
+            exit 1
+        fi
+    fi
+else
+    echo "$lan_netip.3 loghost" >> /etc/hosts
+    grep "$lan_netip.3 loghost" /etc/hosts > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "loghost added to /etc/hosts."
+    else
+        echo "Failed to add loghost to /etc/hosts file"
+        exit 1
+    fi
+fi
+
+EOF
+
 #Install UFW (if necessary) and allow connections to port 80/tcp from anywhere
 
+ssh remoteadmin@server2-mgmt << EOF
+dpkg-query -s ufw 2> /dev/null | grep "installed" > /dev/null 2>&1  
+if [ $? -ne 0 ]; then
+    echo "Installing UFW"
+    apt update > /dev/null 2>&1
+    apt install -y ufw > /dev/null 2>&1
+else
+    echo "UFW already installed."
+fi
+
+ufw_status=$(ufw status | grep "Status:" | awk '{ print $2 }')
+if [ "$ufw_status" = "active" ]; then
+    echo "UFW already enabled!"
+else
+    ufw --force enable
+fi
+
+ufw status | grep "80/tcp" | grep "ALLOW" | grep "any"
+if [ $? -eq 0 ]; then
+    echo "UFW Rule already exists: allow connections to 80/tcp from anywhere"
+else
+    echo "Adding UFW Rule: allow connections to port 80/tcp from anywhere!"
+    ufw allow from any to any port 80 proto tcp
+fi
+EOF
+
 #Install apache2 in its default configuration
+ssh remoteadmin@server2-mgmt << EOF
+dpkg-query -s apache2 2> /dev/null | grep "installed" > /dev/null 2>&1  
+if [ $? -ne 0 ]; then
+    echo "Installing Apache2"
+    apt update > /dev/null 2>&1
+    apt install -y apache2 > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Apache2 was succesfully installed!"
+    else
+        echo "Failed to install Apache2."
+        exit 1
+    fi
+else
+    echo "Apache2 already installed."
+fi
+EOF
 
 #Configure rsyslog on webhost to send logs to loghost by modifying /etc/rsyslog.conf
 #Add a line like this to the end of the rsyslog.conf file: *.* @loghost
+ssh remoteadmin@server1-mgmt << EOF
+grep "\*.\* @loghost" /etc/rsyslog.conf > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "rsyslog is already configured to send logs to loghost!"
+else
+    echo "Configuring rsyslog to send logs to loghost..."
+    echo "*.* @loghost" >> /etc/rsyslog.conf
+    grep "\*.\* @loghost" /etc/rsyslog.conf > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Successfully configured rsyslog to send logs to loghost!"
+        systemctl restart rsyslog
+    else
+        "Failed to configure rsyslog to send logs to loghost."
+        exit 1
+    fi
+fi
+
+EOF
 
 ##### Configuring NMS #####
 
+echo 'Configuring NMS'
+
 #If the remote changes are successful, update the NMS /etc/hosts file to have the name:
-#loghost with 172.16.1.3 and webhost with 172.16.1.4
+#loghost with 192.168.16.3 and webhost with 192.168.16.4
+
+echo "Updating NMS /etc/hosts file..."
+grep "server1" /etc/hosts | awk 'FNR==1' | sed -i 's/.10/.3/;s/server1/loghost/' /etc/hosts 
+grep "server2" /etc/hosts | awk 'FNR==1' | sed -i 's/.11/.4/;s/server2/webhost/' /etc/hosts 
+lines_hostfile=$(grep -E "loghost|webhost" /etc/hosts | wc -l)
+if [ "$lines_hostfile" -eq "2" ]; then
+    echo "/etc/hosts file was successfully updated!"
+else
+    echo "Failed to update /etc/hosts"
+    exit 1
+fi
 
 #Verify you can retrieve the default apache web page from the NMS using firefox
 #With the URL http://webhost
 
-#Verify you can retrieve the logs showing webhost from loghost using the command:
-#ssh remoteadmin@loghost grep webhost /var/log/syslog
+wget -q -O - http://webhost 
+if [ $? -eq 0 ]; then
+    echo "Successfully retrieved default apache web page!"
+else
+    echo "Failed to retrieve the default apache web page."
+    exit 1
+fi
 
-#If the apache server responds properly and the syslog has entreies from webhost,
+#Verify you can retrieve the logs showing webhost from loghost using the command:
+#If the apache server responds properly and the syslog has entries from webhost,
 #Let the user know that the configuration update has succeeded
 #Otherwise, tell them what did not work in a user-friendly way
+
+webhost_in_log=$(ssh remoteadmin@loghost "grep 'webhost' /var/log/syslog")
+if [ -n $webhost_in_log ]; then
+    echo "Configuration update succeeded!"
+else
+    echo "Failed to update configuration. Could not find webhost in logs from loghost."
+    exit 1
+
+
